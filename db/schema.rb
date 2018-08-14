4e132f60b177c16845cb7e3405b557b5d4032ddf
Jessica Hines <jessicahines717@gmail.com>
2018-08-14 13:41:40 -0500

    add image column to dogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814182206) do
+ActiveRecord::Schema.define(version: 20180814184054) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180814182206) do
     t.integer "stamina"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "hydrants", force: :cascade do |t|
