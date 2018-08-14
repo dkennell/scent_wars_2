5a628701b7023eeba56926301e169b0514b02003
Nick Anderson <tetramputechture@gmail.com>
2018-08-14 14:21:49 -0500

    remove visit hydrant

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,4 @@
 class Dog < ApplicationRecord
   has_many :visits
   has_many :hydrants, through :visits
-
-  def visit_hydrant(hydrant_id, stamina_to_use)
-    visit = find_or_create_by(dog_id: id, hydrant_id: hydrant_id)
-    visit.update(stamina_used: visit.stamina_used + stamina_to_use)
-    update(stamina: stamina - stamina_to_use)
-  end
 end
