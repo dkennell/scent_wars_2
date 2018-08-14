9ab3a38adb90d6e4085018d7221bb43eb8faa476
Nick Anderson <tetramputechture@gmail.com>
2018-08-14 14:40:28 -0500

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_dog
-    Dog.find(session[:dog_id])
+    Dog.find_by(id: session[:dog_id])
   end
 end
