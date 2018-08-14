37bf6e956b5c8adde7ffadd16798e80969d36cf5
Nick Anderson <tetramputechture@gmail.com>
2018-08-14 13:39:24 -0500

    edit routes, dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -7,35 +7,23 @@ class DogsController < ApplicationController
     @dogs = Dog.all
   end
 
-  # GET /dogs/1
-  # GET /dogs/1.json
-  def show
-  end
-
-  # GET /dogs/new
-  def new
-    @dog = Dog.new
-  end
-
-  # GET /dogs/1/edit
-  def edit
-  end
-
-  # POST /dogs
-  # POST /dogs.json
   def create
-    @dog = Dog.new(dog_params)
-
-    respond_to do |format|
-      if @dog.save
-        format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
-        format.json { render :show, status: :created, location: @dog }
-      else
-        format.html { render :new }
-        format.json { render json: @dog.errors, status: :unprocessable_entity }
-      end
+    if session[:dog_id].blank?
+      dog = Dog.create({
+        name: 'dog' + SecureRandom.uuid.to_s,
+        level: 1,
+        points: 0,
+        stamina: 100
+      })
+      session[:dog_id] = dog.id
+    else
+      redirect_to show_dog_url(session[:dog_id])
     end
   end
+
+  # GET /dogs/1
+  # GET /dogs/1.json
+  def show; end
 
   # PATCH/PUT /dogs/1
   # PATCH/PUT /dogs/1.json
@@ -62,13 +50,14 @@ class DogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dog
-      @dog = Dog.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def dog_params
-      params.require(:dog).permit(:name, :level, :points, :stamina)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def dog_params
+    params.require(:dog).permit(:name, :level, :points, :stamina)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'dogs#create'
+
   resources :visits
   resources :hydrants
-  resources :dogs
+  resources :dogs, only: %i(create index show update destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
